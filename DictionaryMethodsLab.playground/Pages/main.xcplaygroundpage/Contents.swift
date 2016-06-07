4175de8087d50a059f531d4cd6044da6f57fcb04
JimCampagno <jimcampagno@gmail.com>
2016-06-07 16:36:36 -0400

    add the playground file

---
+++ DictionaryMethodsLab.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -0,0 +1,68 @@
+/*: Outline
+ 
+ 
+ # Iterating over a Dictionary
+ 
+ ### Readings associated with this lab
+ 
+ * [Dictionary](https://github.com/learn-co-curriculum/swift-dictionary-readme)
+ * [Iterating over a Dictionary](https://github.com/learn-co-curriculum/swift-dictionaryIteration-readme)
+ * [Dictionary - Methods](https://github.com/learn-co-curriculum/swift-dictionaryMethods-readme)
+ 
+ 
+
+ */
+/*: question1
+ ### 1. 
+ 
+ */
+// write your code here
+
+
+
+
+/*: question2
+ ### 2. Other question.
+ */
+// write your code here
+
+
+
+
+/*: question3
+ ### 3. Other question.
+ */
+// write your code here
+
+
+
+
+
+/*: question4
+ ### 4. Q
+ */
+
+
+
+
+
+
+
+
+
+
+/*: question5
+ ### 5. QQ
+ */
+
+
+
+
+
+
+
+
+/*:
+ [Solution](solution)
+ */
+// ❤️
